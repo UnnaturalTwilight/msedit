df0df33147b21c7f9b74e5d6c03a1b3b8889e2d3
UnnaturalTwilight <107954129+UnnaturalTwilight@users.noreply.github.com>
2025-12-12 16:34:51 -0500

    Make CRLF checking use platform default when file is a single line

--- crates/edit/src/buffer/mod.rs
+++ crates/edit/src/buffer/mod.rs
@@ -786,7 +786,11 @@ impl TextBuffer {
             }
 
             // We'll assume CRLF if more than half of the lines end in CRLF.
-            let newlines_are_crlf = crlf_count >= lines / 2;
+            let newlines_are_crlf = if lines <= 1 {
+                if crlf_count >= 1 { true } else { cfg!(windows) }
+            } else {
+                crlf_count >= lines / 2
+            };
 
             // We'll assume tabs if there are more lines starting with tabs than with spaces.
             let indent_with_tabs = tab_indentations > space_indentations;
